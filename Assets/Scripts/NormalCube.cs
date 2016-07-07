765cb758c29b23c98417d254c61c49c9b7c331ca
Daochang Liu <finspire13@gmail.com>
2016-07-07 11:34:05 +0800

    Buff Model Updated 2

--- Assets/Scripts/NormalCube.cs
+++ Assets/Scripts/NormalCube.cs
@@ -65,8 +65,8 @@ public class NormalCube : MonoBehaviour, Locatable,Distroyable,ScoreCount {
 
 	void createBuff(){
 
-//		string[] buffList = {"Heal","GhostForm","BombPowerUp","BombNumberUp","FireLifeTimeUp","SpeedUp" };
-		string[] buffList = {"FireLifeTimeUp"};
+		string[] buffList = {"Heal","GhostForm","BombPowerUp","BombNumberUp","FireLifeTimeUp","SpeedUp","SlowDown" };
+		//string[] buffList = {"FireLifeTimeUp"};
 		int buffIndex=UnityEngine.Random.Range (0, buffList.Length);
 
 		GameObject buff = Resources.Load(buffList[buffIndex]) as GameObject;
