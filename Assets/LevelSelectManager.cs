89b8c00835ef7e19315ae549a8f49d3c64ee6091
Wentao MA <mail.walter.ma@gmail.com>
2016-07-08 11:49:14 +0800

    Level select bug fix

--- Assets/LevelSelectManager.cs
+++ Assets/LevelSelectManager.cs
@@ -1,10 +1,13 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class LevelSelectManager : MonoBehaviour {
 
 	public ListViewController CustomMapList;
 	public ListViewController PresetMapList;
+	public Toggle[] LevelToggles;
+	public int DifficulityLevel = 1;
 
 	private string[] presetMaps;
 
@@ -17,6 +20,8 @@ public class LevelSelectManager : MonoBehaviour {
 		CustomMapList.addListContents (MapDataHelper.instance.getCustomMapList(), onCustomCLick);
 		presetMaps = MapDataHelper.instance.getPresetMapList ();
 		PresetMapList.addListContents (presetMaps, onPresetClick);
+		DifficulityLevel = GameManager.instance.level;
+		initToggles ();
 	}
 
 	public void startWithPresetMap(string mapName){
@@ -33,6 +38,13 @@ public class LevelSelectManager : MonoBehaviour {
 
 	public void backToMenu(){
 		GameManager.instance.changeGameState (GameState.start);
+	}
+
+	void initToggles(){
+		foreach (Toggle tg in LevelToggles) {
+			tg.isOn = false;
+		}
+		LevelToggles [DifficulityLevel - 1].isOn = true;
 	}
 
 }
