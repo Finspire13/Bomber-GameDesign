22e6daa1f0c565906437e5d94be2f3d23e92faac
Wentao MA <mail.walter.ma@gmail.com>
2016-07-08 13:08:28 +0800

    Camera angel change

--- Assets/DifficultyLevelSelectController.cs
+++ Assets/DifficultyLevelSelectController.cs
@@ -4,13 +4,10 @@ using System.Collections;
 public class DifficultyLevelSelectController : MonoBehaviour {
 
 	public int level;
-	public LevelSelectManager manager;
 
 	public void DifficultyLevelSelect(bool isSelect){
 		if (isSelect) {
 			GameManager.instance.level = this.level;
-			manager.DifficulityLevel = this.level;
-			Debug.Log ("set Level: " + level);
 		}
 	}
 }
